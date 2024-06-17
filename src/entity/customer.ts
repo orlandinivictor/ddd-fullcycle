bb62fe88fb2e64e1348d949d73c448e69c2e55d1
Victor Orlandini <orlandinivictor@outlook.com>
2024-06-17 10:02:20 -0300

    feat: entity module

---
+++ src/entity/customer.ts
@@ -0,0 +1,62 @@
+/*
+  Entidade - Guarda regras de negócio
+  Entidade Anemica - Guarda apenas dados
+
+  * Uma entidade por padrão sempre deve se auto-validar
+
+  ORM vs DDD
+  DDD -> Entidade focada em negocio;
+  ORM -> Entidade focada em persistencia;
+
+  Domain
+  - Entity
+    - customer.ts(regras de negocio)
+
+  ORM - Infra
+  - Entity / Model
+    - customer.ts(get,set)
+*/
+
+class Customer {
+  _id: string;
+  _name: string = "";
+  _address: string = "";
+  _active: boolean = false;
+
+  constructor(id: string, name: string) {
+    this._id = id;
+    this._name = name;
+
+    this.validate();
+  }
+
+  validate() {
+    if (this._name.length === 0) {
+      throw new Error("Name is required");
+    }
+
+    if (this._id.length === 0) {
+      throw new Error("Id is required");
+    }
+  }
+
+  changeName(name: string) {
+    this._name = name;
+
+    this.validate();
+  }
+
+  activate() {
+    if (this._address.length === 0) {
+      throw new Error("Address is mandatory to activate a customer");
+    }
+
+    this._active = true;
+  }
+
+  deactivate() {
+    this._active = false;
+  }
+}
+
+const customer = new Customer("123", "");
