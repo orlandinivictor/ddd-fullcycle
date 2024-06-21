8b2b36bac6a8b158d9026a4e5f89bf9bcbb09ba3
Victor Orlandini <orlandinivictor@outlook.com>
2024-06-21 06:51:15 -0300

    feat: create generic repository interface

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { Address } from "./entity/address";
-import { Customer } from "./entity/customer";
-import { Order } from "./entity/order";
-import { OrderItem } from "./entity/order-item";
+import { Address } from "./domain/entity/address";
+import { Customer } from "./domain/entity/customer";
+import { Order } from "./domain/entity/order";
+import { OrderItem } from "./domain/entity/order-item";
 
 // Agregados diferentes referenciam-se por ID;
 // Agregados iguais referenciam o próprio valor;
