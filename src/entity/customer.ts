554bb4b9602d8bcd196e7b9ec3acb905753a233d
Victor Orlandini <orlandinivictor@outlook.com>
2024-06-21 06:22:45 -0300

    feat: add place order method to order domain service

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -24,6 +24,7 @@ export class Customer {
   private _name: string = "";
   private _address!: Address;
   private _active: boolean = false;
+  private _rewardPoints: number = 0;
 
   constructor(id: string, name: string) {
     this._id = id;
@@ -32,8 +33,16 @@ export class Customer {
     this.validate();
   }
 
+  get id(): string {
+    return this._id;
+  }
+
   get name(): string {
     return this._name;
+  }
+
+  get rewardPoints(): number {
+    return this._rewardPoints;
   }
 
   isActive(): boolean {
@@ -66,6 +75,10 @@ export class Customer {
 
   deactivate() {
     this._active = false;
+  }
+
+  addRewardPoints(points: number) {
+    this._rewardPoints += points;
   }
 
   set Address(address: Address) {
--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -1,8 +1,20 @@
+import { Customer } from "../entity/customer";
 import { Order } from "../entity/order";
 import { OrderItem } from "../entity/order-item";
 import { OrderService } from "./order.service";
 
 describe("Order service unit tests", () => {
+  it("should place an order", () => {
+    const customer = new Customer("1", "Victor Orlandini");
+    const orderItem1 = new OrderItem("1", "Programação", 3200, "1", 1);
+    const orderItem2 = new OrderItem("2", "Plano Canadá", 1600, "1", 2);
+
+    const order = OrderService.placeOrder(customer, [orderItem1, orderItem2]);
+
+    expect(customer.rewardPoints).toBe(3200);
+    expect(order.total()).toBe(6400);
+  });
+
   it("should get total of all orders", () => {
     const orderItem1 = new OrderItem("1", "Programação", 10, "1", 2);
     const orderItem2 = new OrderItem("2", "Arquitetura", 20, "1", 1);
