2fa56f6719b1cae26cf70c2552c1d9f4febf15f2
Victor Orlandini <orlandinivictor@outlook.com>
2024-06-22 08:38:58 -0300

    feat: add order agregate entities

--- src/domain/entity/order-item.ts
+++ src/domain/entity/order-item.ts
@@ -19,8 +19,20 @@ export class OrderItem {
     this._quantity = quantity;
   }
 
+  get id(): string {
+    return this._id;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
   get price(): number {
     return this._price * this._quantity;
+  }
+
+  get productId(): string {
+    return this._productId;
   }
 
   get quantity(): number {
--- src/domain/entity/order.ts
+++ src/domain/entity/order.ts
@@ -15,6 +15,18 @@ export class Order {
     this.validate();
   }
 
+  get id(): string {
+    return this._id;
+  }
+
+  get customerId(): string {
+    return this._customerId;
+  }
+
+  get items(): OrderItem[] {
+    return this._items;
+  }
+
   validate() {
     if (this._id.length === 0) {
       throw new Error("Id is required");
