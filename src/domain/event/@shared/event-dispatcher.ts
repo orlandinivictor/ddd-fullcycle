b5ab2f51513dd0044072f606deb0fc0ee81ab0b2
Victor Orlandini <orlandinivictor@outlook.com>
2024-06-26 09:05:21 -0300

    feat: implements register method to event dispatcher

--- src/domain/event/@shared/event-dispatcher.ts
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -3,6 +3,12 @@ import { EventHandlerInterface } from "./event-handler.interface";
 import { EventInterface } from "./event.interface";
 
 export class EventDispatcher implements EventDispatcherInterface {
+  private eventHandlers: { [eventName: string]: EventHandlerInterface[] } = {};
+
+  get getEventHandlers(): { [eventName: string]: EventHandlerInterface[] } {
+    return this.eventHandlers;
+  }
+
   notify(event: EventInterface): void {
     throw new Error("Method not implemented.");
   }
@@ -11,7 +17,11 @@ export class EventDispatcher implements EventDispatcherInterface {
     eventName: string,
     eventHandler: EventHandlerInterface<EventInterface>
   ): void {
-    throw new Error("Method not implemented.");
+    if (!this.eventHandlers[eventName]) {
+      this.eventHandlers[eventName] = [];
+    }
+
+    this.eventHandlers[eventName].push(eventHandler);
   }
 
   unregister(
