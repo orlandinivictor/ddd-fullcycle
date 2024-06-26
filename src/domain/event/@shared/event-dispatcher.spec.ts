6aa04f248cb6df249b67a4c51ac37eb6cfa2ddd5
Victor Orlandini <orlandinivictor@outlook.com>
2024-06-26 09:30:00 -0300

    feat: add notify method and its tests

--- src/domain/event/@shared/event-dispatcher.spec.ts
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -1,4 +1,5 @@
 import { SendEmailWhenProductIsCreatedHandler } from "../product/handler/send-email-when-product-is-created.handler";
+import { ProductCreatedEvent } from "../product/product-created.event";
 import { EventDispatcher } from "./event-dispatcher";
 
 describe("Domain events tests", () => {
@@ -50,5 +51,27 @@ describe("Domain events tests", () => {
     expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBe(
       undefined
     );
+  });
+
+  it("should notify all event handlers", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+    const spyEventHandler = jest.spyOn(eventHandler, "handle");
+
+    eventDispatcher.register("ProductCreatedEvent", eventHandler);
+    expect(
+      eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]
+    ).toMatchObject(eventHandler);
+
+    const productCreatedEvent = new ProductCreatedEvent({
+      email: "orlandinivictor@outlook.com",
+      name: "Product 1",
+      description: "Product 1 description",
+      price: 10,
+    });
+    eventDispatcher.notify(productCreatedEvent);
+
+    expect(spyEventHandler).toHaveBeenCalled();
+    expect(spyEventHandler).toHaveBeenCalledTimes(1);
   });
 });
