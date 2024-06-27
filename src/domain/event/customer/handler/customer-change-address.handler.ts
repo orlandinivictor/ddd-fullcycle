6845a9c1fd20ca274a1aef0f3886a745611de08d
Victor Orlandini <orlandinivictor@outlook.com>
2024-06-27 16:19:39 -0300

    fix: add specific event type to handlers

--- src/domain/event/customer/handler/customer-change-address.handler.ts
+++ src/domain/event/customer/handler/customer-change-address.handler.ts
@@ -1,7 +1,10 @@
 import { EventHandlerInterface } from "../../@shared/event-handler.interface";
 import { EventInterface } from "../../@shared/event.interface";
+import { CustomerChangedAddressEvent } from "../customer-changed-address.event";
 
-export class CustomerChangeAddressHandler implements EventHandlerInterface {
+export class CustomerChangeAddressHandler
+  implements EventHandlerInterface<CustomerChangedAddressEvent>
+{
   handle(event: EventInterface): void {
     console.log(
       `Endereço do cliente: ${event.eventData.id}, ${event.eventData.nome} alterado para: ${event.eventData.endereco}`
